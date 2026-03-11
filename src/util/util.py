30e328e17d6dfde89141474a08d8cba4807e84de
arunPdl02 <aroon.paudel@gmail.com>
2026-03-11 15:25:17 -0700

    added a utility function that copies directory content of static to public

---
+++ src/util/util.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+dir_path_static = "./static"
+dir_path_public = "./static"
+logger = []
+
+def copy_static_to_public():
+    global logger
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+        logger.append("removing public directory")
+    os.mkdir(dir_path_public)
+    copy_dir_r(dir_path_static, dir_path_public)
+
+    # print(logger)
+
+
+def copy_dir_r(src, dest):
+    global logger
+    logger.append(f"inside {src} dir")
+    content = os.listdir(src)
+    for file in content:
+        src_file_path = os.path.join(src, file)
+        dest_file_path = os.path.join(dest, file)
+        if os.path.isfile(src_file_path):
+            shutil.copy(src_file_path, dest_file_path)
+            logger.append(f"copying file {file} from {src} to {dest}")
+        
+        if os.path.isdir(src_file_path):
+            os.mkdir(dest_file_path)
+            logger.append(f"making {dest_file_path} directory")
+            copy_dir_r(src_file_path, dest_file_path)
